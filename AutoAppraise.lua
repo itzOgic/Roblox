b27162eef278652d7141fafe597dd69087788ef9
itzOgic <120444147+itzOgic@users.noreply.github.com>
2024-11-04 20:41:28 +0700

    Update AutoAppraise.lua

--- AutoAppraise.lua
+++ AutoAppraise.lua
@@ -1,42 +1,61 @@
 local SolarisLib = loadstring(game:HttpGet"https://raw.githubusercontent.com/itzOgic/Roblox/main/SolarisLib.lua")()
 local player = game.Players.LocalPlayer
-local statFolder,Filtered,tools
+local statFolder,Filtered
 local WeightVal,DelayVal = 100, 0.8
+local fishDone = {}
 local MutList = {"Aurora", "Mythical", "Midas", "Ghastly", "Sinister", "Mosaic", "Electric", "Glossy", "Silver", "Darkened", "Translucent", "Frozen", "Albino", "Negative"}
 local win = SolarisLib:New({
-  Name = "Fisch Auto Appraise by itzOgic",
-  FolderToSave = "SolarisLibStuff"
+  	Name = "Fisch Auto Appraise by itzOgic",
+  	FolderToSave = "SolarisLibStuff"
 })
 
 local tab = win:Tab("Auto Appraise")
 local sec = tab:Section("Menu")
 
-local AutoToggle = sec:Toggle("Auto Appraise Toggle", false, "AutoToggle", function(t) end)
-local AppraiseDelay = sec:Textbox("Appraise Delay", false, function(t) DelayVal = t end)
-local WeightToggle = sec:Toggle("Weight Filter Toggle", false, "WeightToggle", function(t) end)
-local WeightBox = sec:Textbox("Weight Target", false, function(t) WeightVal = t end)
-local SparklingToggle = sec:Toggle("Require Sparkling", false, "SparklingToggle", function(t) end)
-local ShinyToggle = sec:Toggle("Require Shiny", false, "ShinyToggle", function(t) end)
-local MutationToggle = sec:Toggle("Mutation Toggle", false, "MutationToggle", function(t) end)
-local MutationList = sec:MultiDropdown("Mutation Target", MutList, {"Glossy" ,"Ghastly"},"MutationList", function(t) end)
-WeightBox:Set("100")
-AppraiseDelay:Set("0.8")
-
-local function has_value(tab, val)
-	for index, value in ipairs(tab) do		
-		if type(value) ~= "table" and string.find(tostring(value), tostring(val)) then
-			return true
-		elseif type(value) == "table" and value.Name and string.find(value.Name, val) then
-			return true
-		end
+local function hasValue(tab, val)
+	for index, value in next, tab do		
+		if val == value then return true end
 	end
 	return false
 end
 
-local function getTools()
-	for i,v in pairs(player.Character:GetChildren()) do
-		if v:isA("Tool") then
-			return v
+local function getallOwnedFish()
+	local OwnedList = {}
+	for i, fish in next, player.Backpack:GetChildren() do
+		if not hasValue(OwnedList,fish.Name) then
+			table.insert(OwnedList, fish.Name)
+		end
+	end
+	return OwnedList
+end
+
+local allOwnedFish = getallOwnedFish()
+local AutoToggle = sec:Toggle("Auto Appraise Toggle", false, "AutoToggle", function() end)
+local AppraiseAll = sec:Toggle("Appraise all Selected Fish", false, "AppraiseAll", function() end)
+local FishSelection = sec:MultiDropdown("Selected Fish", allOwnedFish, {},"FishSelection", function() end)
+local AppraiseDelay = sec:Textbox("Appraise Delay", false, function(t) DelayVal = t end)
+local WeightToggle = sec:Toggle("Weight Filter Toggle", false, "WeightToggle", function() end)
+local WeightBox = sec:Textbox("Weight Target", false, function(t) WeightVal = t; fishDone = {} end)
+local SparklingToggle = sec:Toggle("Require Sparkling", false, "SparklingToggle", function() fishDone = {} end)
+local ShinyToggle = sec:Toggle("Require Shiny", false, "ShinyToggle", function() fishDone = {} end)
+local MutationToggle = sec:Toggle("Mutation Toggle", false, "MutationToggle", function() end)
+local MutationList = sec:MultiDropdown("Mutation Target", MutList, {"Glossy" ,"Ghastly"},"MutationList", function() fishDone = {} end)
+WeightBox:Set("100")
+AppraiseDelay:Set("0.8")
+
+local function switchFish()
+	for i,fish in next, player.Backpack:GetChildren() do
+		if hasValue(FishSelection.Value,fish.Name) and not hasValue(fishDone,fish) then
+			player.PlayerGui.hud.safezone.backpack.events.equip:FireServer(fish)
+			return
+		end
+	end
+end
+
+local function getHeldFish()
+	for i,v in next, player.Character:GetChildren() do
+		if v:isA("Tool") then 
+			return v 
 		end
 	end
 	return nil
@@ -45,7 +64,7 @@ end
 player.Backpack.ChildAdded:Connect(function(instance)
 	if instance:isA("Tool") and not instance:FindFirstChild("link") then
 		repeat task.wait() until instance:FindFirstChild("link")
-		local oldtools = getTools()
+		local oldtools = getHeldFish()
 		if oldtools then oldtools.Parent = player.Backpack end
 		if AutoToggle then
 			player.PlayerGui.hud.safezone.backpack.events.equip:FireServer(instance)
@@ -54,29 +73,19 @@ player.Backpack.ChildAdded:Connect(function(instance)
 end)
 
 local function applyFilter(statFolder)
-	if WeightToggle.Value then
-		if statFolder.Weight.Value < tonumber(WeightVal) then
-			return false
-		end
+	if WeightToggle.Value and statFolder.Weight.Value < tonumber(WeightVal) then
+		return false
 	end
-	if SparklingToggle.Value then
-		if not statFolder:FindFirstChild("Sparkling") then
-			return false
-		end
+	if SparklingToggle.Value and not statFolder:FindFirstChild("Sparkling") then
+		return false
 	end
-	if ShinyToggle.Value then
-		if not statFolder:FindFirstChild("Shiny") then
-			return false
-		end
+	if ShinyToggle.Value and not statFolder:FindFirstChild("Shiny") then
+		return false
 	end
 	if MutationToggle.Value then
 		local Mutation = statFolder:FindFirstChild("Mutation")
-		
-		if not Mutation then
-			return false 
-		end
-		
-		if not has_value(MutationList.Value, Mutation.Value) then
+		if not Mutation then return false end
+		if not hasValue(MutationList.Value, Mutation.Value) then
 			return false
 		end
 	end
@@ -85,13 +94,21 @@ end
 
 local function AutoAppraise()
 	if not AutoToggle.Value then return end
-	tools = getTools()
-	if tools ~= nil and AutoToggle.Value then
-		statFolder = tools.link.Value
+	local fish = getHeldFish()
+	if fish then
+		statFolder = fish.link.Value
 		Filtered = applyFilter(statFolder)
-		if Filtered then return end
+		if Filtered then 
+			if AppraiseAll.Value and not hasValue(fishDone,fish) then
+				table.insert(fishDone,fish)
+				switchFish()
+			end
+			return
+		end
 		workspace.world.npcs.Appraiser.appraiser.appraise:InvokeServer()
 		task.wait(tonumber(DelayVal))
+	elseif not fish and AppraiseAll.Value then
+		switchFish()
 	end
 end
 
