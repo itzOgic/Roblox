765fc2756904ae7d5916b6096478c87548f922f6
itzOgic <120444147+itzOgic@users.noreply.github.com>
2024-11-10 22:37:41 +0700

    Update AutoAppraise.lua

--- AutoAppraise.lua
+++ AutoAppraise.lua
@@ -37,10 +37,10 @@ local FishSelection = sec:MultiDropdown("Selected Fish", allOwnedFish, {},"FishS
 local AppraiseDelay = sec:Textbox("Appraise Delay", false, function(t) DelayVal = t end)
 local WeightToggle = sec:Toggle("Weight Filter Toggle", false, "WeightToggle", function() end)
 local WeightTarget = sec:MultiDropdown("Weight Target", {"Big","Giant"}, {"Big","Giant"},"WeightTarget", function() fishDone = {} end)
-local SparklingToggle = sec:Toggle("Require Sparkling", false, "SparklingToggle", function() fishDone = {} end)
-local ShinyToggle = sec:Toggle("Require Shiny", false, "ShinyToggle", function() fishDone = {} end)
 local MutationToggle = sec:Toggle("Mutation Toggle", false, "MutationToggle", function() end)
 local MutationList = sec:MultiDropdown("Mutation Target", MutList, {"Any"},"MutationList", function() fishDone = {} end)
+local SparklingToggle = sec:Toggle("Require Sparkling", false, "SparklingToggle", function() fishDone = {} end)
+local ShinyToggle = sec:Toggle("Require Shiny", false, "ShinyToggle", function() fishDone = {} end)
 AppraiseDelay:Set("0.8")
 
 local function switchFish()
